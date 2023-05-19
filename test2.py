dde92b079b16c93b15e5b64b7b12d2d7fcd6ba76
Bughehehe <olekk31@gmail.com>
2023-05-19 21:21:11 +0200

    changed files

--- test2.py
+++ test2.py
@@ -1,13 +1,9 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
 
-G = nx.barabasi_albert_graph(20,5)
+G = nx.erdos_renyi_graph(3, 2)
 
-# nx.draw_spring(G)
 nx.draw(G)
-# nx.draw_planar(G)
 plt.figure(2)
 plt.hist(nx.centrality.closeness_centrality(G).values())
 plt.show()
